f4d06af94c166e9f5086834d791f3c61bc460d30
Chad Whitacre <chadwhitacre@sentry.io>
2024-01-26 16:15:29 -0500

    Send anything to Sentry from the sink

--- src/sinks/sentry/metrics/sink.rs
+++ src/sinks/sentry/metrics/sink.rs
@@ -1,6 +1,10 @@
 use async_trait::async_trait;
 use bytes::BytesMut;
 use futures::{stream::BoxStream, StreamExt};
+use sentry::metrics::FractionUnit;
+use sentry::metrics::Metric;
+use std::env;
+use sysinfo::System;
 use tokio::{io, io::AsyncWriteExt};
 use tokio_util::codec::Encoder as _;
 use vector_lib::codecs::encoding::Framer;
@@ -29,6 +33,12 @@ where
     T: io::AsyncWrite + Send + Sync + Unpin,
 {
     async fn run(mut self: Box<Self>, mut input: BoxStream<'_, Event>) -> Result<(), ()> {
+        let sentry_dsn = env::var("SENTRY_DSN").expect("SENTRY_DSN not set");
+        let _guard = sentry::init((sentry_dsn, sentry::ClientOptions {
+          release: sentry::release_name!(),
+          ..Default::default()
+        }));
+
         let bytes_sent = register!(BytesSent::from(Protocol("sentry_metrics".into(),)));
         let events_sent = register!(EventsSent::from(Output(None)));
         while let Some(mut event) = input.next().await {
@@ -41,6 +51,11 @@ where
                 // Error is handled by `Encoder`.
                 finalizers.update_status(EventStatus::Errored);
             })?;
+
+            let mut sys = System::new_all();
+            sys.refresh_memory();
+            let memory = sys.used_memory() as f64 / sys.total_memory() as f64;
+            Metric::gauge("memory", memory).with_unit(FractionUnit::Ratio).send();
 
             match self.output.write_all(&bytes).await {
                 Err(error) => {
