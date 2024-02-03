e82218ca6cc022c376456d177bbb80eaf725c7e4
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2024-02-03 18:03:10 +0100

    add tags, fix name and timestamp

--- src/sinks/sentry/metrics/sink.rs
+++ src/sinks/sentry/metrics/sink.rs
@@ -1,15 +1,36 @@
 use async_trait::async_trait;
 use futures::{stream::BoxStream, StreamExt};
 use futures_util::future::ready;
-use sentry::metrics::Metric;
+use sentry::metrics::{Metric, MetricBuilder};
+use std::time::{Duration, UNIX_EPOCH};
 
 use crate::{
-    event::{Event, MetricValue},
+    event::{Event, Metric as SourceMetric, MetricValue},
     sinks::util::StreamSink,
 };
 
 pub struct SentryMetricsSink {
     pub dsn: String,
+}
+
+fn finish_metric(source: &SourceMetric, mut sentry_metric: MetricBuilder) {
+    if let Some(tags) = source.series().tags() {
+        for (k, v) in tags.iter_all() {
+            if let Some(v) = v {
+                sentry_metric = sentry_metric.with_tag(k.to_owned(), v.to_owned());
+            }
+        }
+    }
+
+    if let Some(time) = source.data().time.timestamp {
+        if let Ok(time) = u64::try_from(time.timestamp()) {
+            let system_time = UNIX_EPOCH + Duration::from_secs(time);
+            // XXX: why does the SDK need a SystemTime here?
+            sentry_metric = sentry_metric.with_time(system_time);
+        }
+    }
+
+    sentry_metric.send();
 }
 
 #[async_trait]
@@ -23,25 +44,36 @@ impl StreamSink<Event> for SentryMetricsSink {
         let _guard = sentry::init(self.dsn);
 
         while let Some(metric) = input.next().await {
-            let name = metric.series().name().name.clone();
+            let series = metric.series();
+            let series_name = series.name();
+            let mut name = series_name.namespace.clone().unwrap_or_default();
+            if !name.is_empty() {
+                name.push('.');
+            }
+
+            name.push_str(&series_name.name);
+
             match metric.data().value() {
                 MetricValue::Counter { value } => {
-                    Metric::incr(name, *value).send();
+                    finish_metric(&metric, Metric::incr(name, *value));
                 }
                 MetricValue::Gauge { value } => {
-                    Metric::gauge(name, *value).send();
+                    finish_metric(&metric, Metric::gauge(name, *value));
                 }
                 MetricValue::Set { values } => {
                     for value in values {
                         // XXX: why not submit the entire set at once?
-                        Metric::set(name.clone(), &value).send();
+                        finish_metric(&metric, Metric::set(name.clone(), &value));
                     }
                 }
                 MetricValue::Distribution { samples, .. } => {
                     for sample in samples {
                         for _ in 0..sample.rate {
                             // XXX: sentry should allow me to submit value + count
-                            Metric::distribution(name.clone(), sample.value).send();
+                            finish_metric(
+                                &metric,
+                                Metric::distribution(name.clone(), sample.value),
+                            );
                         }
                     }
                 }
